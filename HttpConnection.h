b8eb41f73f9892eeef543d5f1e4da1ef36e98bd7
intersoulest <2358627482@qq.com>
2024-06-28 18:04:20 +0800

    four commit

--- HttpConnection.h
+++ HttpConnection.h
@@ -1,14 +1,18 @@
 #include "const.h"
+#include<unordered_map>
 class HttpConnection:public std::enable_shared_from_this<HttpConnection>{
     friend class LogicSystem;
 public:
     HttpConnection(tcp::socket socket);
     void Start();
+    std::string _get_url;
+    std::unordered_map<std::string, std::string> _get_params;
+    void PreParseGetParam();
 private:
     void CheckDeadline();
     void WriteResponse();
     void HandleReq();
-
+    
     tcp::socket _socket;
     // 用来接受数据
     beast::flat_buffer  _buffer{ 8192 };
@@ -19,4 +23,5 @@ private:
     // 用来做定时器判断请求是否超时
     net::steady_timer deadline_{
         _socket.get_executor(), std::chrono::seconds(60) };
+
 };
--- LogicSystem.h
+++ LogicSystem.h
@@ -2,7 +2,6 @@
 #include "functional"
 #include "map"
 #include "const.h"
-
 class HttpConnection;
 typedef std::function<void(std::shared_ptr<HttpConnection>)> HttpHandler;
 class LogicSystem:public Singleton<LogicSystem>{
@@ -10,7 +9,9 @@ class LogicSystem:public Singleton<LogicSystem>{
 public:
     ~LogicSystem();
     bool HandleGet(std::string, std::shared_ptr<HttpConnection>);
+    bool HandlePost(std::string, std::shared_ptr<HttpConnection>);
     void RegGet(std::string,HttpHandler handler);
+    void RegPost(std::string,HttpHandler handler);
 private:
     LogicSystem();
     std::map<std::string, HttpHandler> _post_handlers;
